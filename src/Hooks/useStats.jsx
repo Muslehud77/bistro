6c1e4d7e4919efb12b8512ad1ac3b76a6caeb6f6
Muslehud77 <fardinmohit@gmail.com>
2023-11-24 03:09:31 +0800

    added all stats for admin

--- src/Hooks/useStats.jsx
+++ src/Hooks/useStats.jsx
@@ -4,18 +4,32 @@ import useContextInfo from "./useContextInfo";
 
 
 const useStats = () => {
-    const {user,enableFetch} = useContextInfo()
+    const {enableFetch} = useContextInfo()
     const axiosSecure = useAxiosSecure()
 
     const {data:stats={},refetch} = useQuery({
         queryKey:['stats'],
         enabled:enableFetch,
         queryFn: async ()=>{
-            const res = await axiosSecure("/admin-stats");
+            const res = await axiosSecure.get("/admin-stats");
             return res.data
         }
     })
-    return {stats,refetch}
+
+    const {data:itemsSold=[]}= useQuery({
+        queryKey:['itemsSold'],
+        enabled:enableFetch,
+        queryFn: async ()=>{
+            const res = await axiosSecure.get("/items-sold")
+            const soldItems = res.data.map(item => ({...item,revenue:item.revenue.toFixed(2)}));
+     
+            return res.data;
+        }
+    })
+
+    
+
+    return {stats,itemsSold,refetch}
 };
 
 export default useStats;
